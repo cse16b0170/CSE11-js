dc9ca2a9d29067173dbfac146509126cc7261d55
cse16b0170 <a9475210@gmail.com>
2026-02-24 10:27:47 +0530

    commit for loop

---
+++ loops/forloop/script.js
@@ -0,0 +1,7 @@
+var count;
+document.write("starting loop"+"<br>");
+for(count=0;count<5;count++){
+    document.write("Current Count:"+count);
+    document.write("<br>");
+}
+document.write("Loop ended");
