ff32a079b62e2c6366a2b19fa77fd8278326cdf6
cse16b0170 <a9475210@gmail.com>
2026-02-27 12:06:45 +0530

    commit swichcase

---
+++ loops/forloop/swich/script.js
@@ -0,0 +1,20 @@
+var grade = "c";
+switch(grade){
+    case "A":
+        console.log("Excellent");
+        break;
+    case "B":
+        console.log("Good");
+        break;
+    case "C":
+        console.log("Fair");
+        break;
+    case "D":
+        console.log("Poor");
+        break;
+    case "F":
+        console.log("Fail");
+        break;
+    default:
+        console.log("Invalid grade");
+}
