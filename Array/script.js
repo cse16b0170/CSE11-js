3d6b5c5bc87b1f39926e6fdf1ff707bbb173f52f
cse16b0170 <a9475210@gmail.com>
2026-03-10 09:36:51 +0530

    commit object and array

---
+++ Array/script.js
@@ -0,0 +1,16 @@
+let collection =[
+    {},
+    [],
+    true,
+    "john",
+    function () {},
+    9559194680,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+
+var trainerinfo=["Aviral",18,true]; 
+console.log(trainerinfo);
---
+++ object/script.js
@@ -0,0 +1,12 @@
+var person ={
+    name:"Aviral",
+    age:18,
+    isStudent:true,
+    address:{
+        city:Kanpur,
+        country:India
+    }
+
+};
+console.log(person.name);
+// Array is a special type of object that can hold multiple values in a single variable.
